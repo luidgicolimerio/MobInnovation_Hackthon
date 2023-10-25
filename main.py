514d59bca3b87f306d637f0486cccc6a3bf31afc
luidgicolimerio <luidgicolimerio@gmail.com>
2023-10-25 17:39:54 -0300

    tiny and kv tests

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # App principal
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from tinydb import TinyDB
+
+db = TinyDB('database.json')
+
+class MyBoxLayout(BoxLayout):
+    
+    def cadastrar(self, *args):
+        user_text = self.ids.user_input.text
+        password_text = self.ids.password_input.text
+        db.insert(
+            {
+              'user': user_text,
+              'password': password_text
+              }
+        )
+
+class MainApp(App):  
+    def build(self):
+        return MyBoxLayout()
+    
+MainApp().run()
